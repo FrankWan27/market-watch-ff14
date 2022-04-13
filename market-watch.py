1a49b00a798553592c5c42a94e0cb744f0aae81a
FrankWan27 <frankwan27@gmail.com>
2022-04-13 00:53:34 -0700

    fix shit

--- market-watch.py
+++ market-watch.py
@@ -6,7 +6,7 @@ import sqlite3 as sl
 #TODO: 99x quanitity, trade volume, cheapest world
 WORLD = 'Cactuar'
 DATACENTER = 'Aether'
-BATCHSIZE = 1000
+BATCHSIZE = 100
 
 con = sl.connect('market.db')
 
@@ -19,6 +19,7 @@ def constructHTTP(ids: list[int], context: str, start: int, listings = 0, hq = "
 def getWorldPrices(ids: list[int]):
     index = 0
     while(index < len(ids)):
+        print("\rQuerying " + str(index) + " - " + str(index + BATCHSIZE) + "...")
         url = constructHTTP(ids, WORLD, index)
         response = requests.get(url)
         
@@ -29,24 +30,26 @@ def getWorldPrices(ids: list[int]):
                     con.execute("""
                         INSERT INTO Prices (id, high_quality, world_price, trade_velocity) 
                         VALUES (?, ?, ?, ?)
-                        ON CONFLICT(id_quality) 
+                        ON CONFLICT(id, high_quality) 
                         DO UPDATE SET 
                             world_price=excluded.world_price, 
-                            trade_velocity=excluded.world_price;
+                            trade_velocity=excluded.trade_velocity;
                     """, (item['itemID'], 0, item['minPriceNQ'], item['nqSaleVelocity']))
                     con.execute("""
                         INSERT INTO Prices (id, high_quality, world_price, trade_velocity) 
                         VALUES (?, ?, ?, ?)
-                        ON CONFLICT(id_quality) 
+                        ON CONFLICT(id, high_quality) 
                         DO UPDATE SET 
                             world_price=excluded.world_price, 
-                            trade_velocity=excluded.world_price;
+                            trade_velocity=excluded.trade_velocity;
                     """, (item['itemID'], 1, item['minPriceHQ'], item['hqSaleVelocity']))
         index += BATCHSIZE
 
 def getDCPrices(ids: list[int]):
     index = 0
     while(index < len(ids)):
+        print("\rQuerying " + str(index) + " - " + str(index + BATCHSIZE) + "...")
+
         url = constructHTTP(ids, DATACENTER, index, listings=1, hq='false')
         response = requests.get(url)
         
@@ -56,9 +59,9 @@ def getDCPrices(ids: list[int]):
                 if(item['listings']):
                     with con:
                         con.execute("""
-                            UPSERT INTO Prices (id, high_quality, dc_price, cheapest_world) 
+                            INSERT INTO Prices (id, high_quality, dc_price, cheapest_world) 
                             VALUES (?, ?, ?, ?)
-                            ON CONFLICT(id_quality) 
+                            ON CONFLICT(id, high_quality) 
                             DO UPDATE SET 
                                 dc_price=excluded.dc_price, 
                                 cheapest_world=excluded.cheapest_world;
@@ -73,9 +76,9 @@ def getDCPrices(ids: list[int]):
                 if(item['listings']):
                     with con:
                         con.execute("""
-                            UPSERT INTO Prices (id, high_quality, dc_price, cheapest_world) 
+                            INSERT INTO Prices (id, high_quality, dc_price, cheapest_world) 
                             VALUES (?, ?, ?, ?)
-                            ON CONFLICT(id_quality) 
+                            ON CONFLICT(id, high_quality) 
                             DO UPDATE SET 
                                 dc_price=excluded.dc_price, 
                                 cheapest_world=excluded.cheapest_world;
@@ -83,6 +86,16 @@ def getDCPrices(ids: list[int]):
 
 
         index += BATCHSIZE
+
+
+def calculateDifferences():
+    with con:
+        con.execute("DROP VIEW IF EXISTS PriceDiff;")
+        con.execute("""
+            CREATE VIEW PriceDiff AS
+            SELECT id, high_quality, cheapest_world, (world_price-dc_price) as profit, trade_velocity
+            FROM Prices;
+        """)
 
 def itemIdToName(id: int) -> str:
     response = requests.get('https://xivapi.com/item/' + str(id) + '?columns=Name')
@@ -93,10 +106,12 @@ def itemIdToName(id: int) -> str:
 
 def updatePrices():
     ids = requests.get('https://universalis.app/api/marketable').json()
+    print(str(len(ids)) + ' items to be queried\n')
     worldPrices = getWorldPrices(ids)
-    print('World Prices Updated')
+    print('World Prices Updated\n')
     dcPrices = getDCPrices(ids)
-    print('DC Prices Updated')
+    print('DC Prices Updated\n')
+    calculateDifferences()
     
     
 updatePrices()
